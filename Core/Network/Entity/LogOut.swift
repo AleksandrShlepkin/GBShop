97376917d227331d8d8a122c955418f92a788a9a
White_Bishop <Shlepkinaleksandr@mail.ru>
2022-02-16 14:47:26 +0300

    added folder and logic auth users

---
+++ Core/Network/Entity/LogOut.swift
@@ -0,0 +1,12 @@
+//
+//  LogOut.swift
+//  GBShop
+//
+//  Created by Mac on 16.02.2022.
+//
+
+import Foundation
+
+struct LogOut: Codable {
+    let result: Int
+}
--- ViewController.swift
+++ ViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+let requestFactory = RequestFactory()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    func auth() {
+        let auth = requestFactory.makeAuthRequestFatory()
+        auth.login(userName: "Somebody", password: "Mypassword") { response  in
+            switch response.result {
+            case .success(let login):
+                print(login)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    
 
 
 }
