7a41cf66f931f3f3f0b1832c99c726be2d34f4e9
White_Bishop <Shlepkinaleksandr@mail.ru>
2022-03-16 17:47:32 +0300

    bug

---
+++ Core/Network/Entity/CommentsModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentsModel.swift
+//  GBShop
+//
+//  Created by Mac on 09.03.2022.
+//
+
+import Foundation
---
+++ Core/Network/Entity/CommentsResponse.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultComment.swift
+//  GBShop
+//
+//  Created by Mac on 16.03.2022.
+//
+
+import Foundation
+
+struct CommentsResponse: Codable {
+    var result: Int?
+    var errorMessage: String?
+    var messege: String?
+}
